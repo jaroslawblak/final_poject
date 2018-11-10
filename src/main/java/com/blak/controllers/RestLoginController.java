3963b3ff5a4c6b674b350ec0b3a7fd5bf4f17313
Jaroslaw Blak <jaroslaw.blak@gmail.com>
2018-11-10 17:05:51 +0100

    Added Login controller and basic auth config

---
+++ src/main/java/com/blak/controllers/RestLoginController.java
@@ -0,0 +1,33 @@
+package com.blak.controllers;
+
+
+import com.blak.model.User;
+import com.blak.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.Base64;
+
+@RestController
+@CrossOrigin
+public class RestLoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/login")
+    public boolean login(@RequestBody User user) {
+        return user.getEmail().equals("user") && user.getPassword().equals("password");
+    }
+
+    @RequestMapping("/user")
+    public Principal user(HttpServletRequest request) {
+        String authToken = request.getHeader("Authorization").substring("Basic".length()).trim();
+        return () -> new String(Base64.getDecoder().decode(authToken)).split(":")[0];
+    }
+}
